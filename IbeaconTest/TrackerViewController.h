0dcc0f5fb0c5dc7db1c73443857a9fe214288fa8
rafabertholdo <rafabertholdo@gmail.com>
2014-07-24 23:19:58 -0300

    First commit

---
+++ IbeaconTest/TrackerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrackerViewController.h
+//  IbeaconTest
+//
+//  Created by Rafael Bertholdo on 7/24/14.
+//  Copyright (c) 2014 Bertholdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrackerViewController : UIViewController
+
+@end
---
+++ IbeaconTest/TransmitterViewController.h
@@ -0,0 +1,13 @@
+//
+//  TransmitterViewController.h
+//  IbeaconTest
+//
+//  Created by Rafael Bertholdo on 7/24/14.
+//  Copyright (c) 2014 Bertholdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransmitterViewController : UIViewController
+
+@end
